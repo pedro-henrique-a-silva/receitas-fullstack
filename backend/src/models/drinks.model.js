fcc5bd4f8f51d38e2d9b93ddddb42916aa24eeca
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-19 10:47:59 -0300

    Criando função de busca por todas categórias

--- backend/src/models/drinks.model.js
+++ backend/src/models/drinks.model.js
@@ -6,6 +6,12 @@ const getAll = async () => {
   return drinks;
 }
 
+const getAllCategories = async () => {
+  const [categories] = await connection.execute('SELECT * FROM drinks_categories');
+  return categories;
+}
+
 module.exports = {
   getAll,
+  getAllCategories,
 }
