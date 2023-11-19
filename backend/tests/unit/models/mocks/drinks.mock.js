8da9d07078aeac988e637c9ca69d7465ab6d8c4e
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-19 12:18:43 -0300

    mock drinks pesquisa por categória

--- backend/tests/unit/models/mocks/drinks.mock.js
+++ backend/tests/unit/models/mocks/drinks.mock.js
@@ -1023,7 +1023,52 @@ const allDrinksCategoriesMockFromDB = [
   }
 ]
 
+const mealsFilteredByCategoryFromDB = [
+  {
+    "idDrink": 13128,
+    "strDrink": "Diesel",
+    "idCategory": 10,
+    "strAlcoholic": "Alcoholic",
+    "strGlass": "Pint glass",
+    "strInstructions": "Pour the lager first then add the blackcurrant cordial. Top up with the cider. The colour sholud be very dark approaching the colour of Guiness.",
+    "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/sxrrqq1454512852.jpg",
+    "strIngredient1": "Lager",
+    "strIngredient2": "Cider",
+    "strIngredient3": "Blackcurrant cordial",
+    "strIngredient4": null,
+    "strIngredient5": null,
+    "strIngredient6": null,
+    "strIngredient7": null,
+    "strIngredient8": null,
+    "strIngredient9": null,
+    "strIngredient10": null,
+    "strIngredient11": null,
+    "strIngredient12": null,
+    "strIngredient13": null,
+    "strIngredient14": null,
+    "strIngredient15": null,
+    "strMeasure1": "1/2 pint ",
+    "strMeasure2": "1/2 pint ",
+    "strMeasure3": "1 dash ",
+    "strMeasure4": null,
+    "strMeasure5": null,
+    "strMeasure6": null,
+    "strMeasure7": null,
+    "strMeasure8": null,
+    "strMeasure9": null,
+    "strMeasure10": null,
+    "strMeasure11": null,
+    "strMeasure12": null,
+    "strMeasure13": null,
+    "strMeasure14": null,
+    "strMeasure15": null,
+    "id": 10,
+    "categoryName": "Beer"
+  }
+]
+
 module.exports = {
   allDrinksMockFromDB,
   allDrinksCategoriesMockFromDB,
+  mealsFilteredByCategoryFromDB,
 }
