52d79f321a817bfe938423f1c6d3b2d0e18d129e
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-19 10:48:55 -0300

    Fazendo teste da busca por todas as categórias

--- backend/tests/unit/models/drinks.test.js
+++ backend/tests/unit/models/drinks.test.js
@@ -1,7 +1,10 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const connection = require('../../../src/models/connection');
-const {allDrinksMockFromDB} = require('./mocks/drinks.mock');
+const {
+  allDrinksMockFromDB,
+  allDrinksCategoriesMockFromDB
+} = require('./mocks/drinks.mock');
 const {drinksModel} = require('../../../src/models/index')
 
 describe('Testando Bebidas - MODEL', () => {
@@ -11,6 +14,14 @@ describe('Testando Bebidas - MODEL', () => {
 
     expect(allDrinks).to.be.an('array');
     expect(allDrinks).to.have.lengthOf(25);
+  })
+
+  it('Testando busca de todas as categórias de receitas de bebidas', async () => {
+    sinon.stub(connection, 'execute').resolves([allDrinksCategoriesMockFromDB])
+    const allDrinksCategories = await drinksModel.getAllCategories();
+
+    expect(allDrinksCategories).to.be.an('array');
+    expect(allDrinksCategories).to.have.lengthOf(11);
   })
 
   afterEach(() => {
