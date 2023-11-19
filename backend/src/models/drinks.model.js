4159df921dbd863f1f1a6d52f2d89a2819728a2d
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-19 11:46:10 -0300

    aplicando camelização as respostas do banco

--- backend/src/models/drinks.model.js
+++ backend/src/models/drinks.model.js
@@ -1,14 +1,14 @@
 const connection = require('./connection');
-
+const camelize = require('camelize');
 
 const getAll = async () => {
   const [drinks] = await connection.execute('SELECT * FROM drinks');
-  return drinks;
+  return camelize(drinks);
 }
 
 const getAllCategories = async () => {
   const [categories] = await connection.execute('SELECT * FROM drinks_categories');
-  return categories;
+  return camelize(categories);
 }
 
 module.exports = {
--- backend/src/models/meals.model.js
+++ backend/src/models/meals.model.js
@@ -1,14 +1,15 @@
 const connection = require('./connection');
+const camelize = require('camelize');
 
 
 const getAll = async () => {
   const [meals] = await connection.execute('SELECT * FROM meals');
-  return meals;
+  return camelize(meals);
 }
 
 const getAllCategories = async () => {
   const [categories] = await connection.execute('SELECT * FROM meals_categories');
-  return categories
+  return camelize(categories);
 }
 
 module.exports = {
