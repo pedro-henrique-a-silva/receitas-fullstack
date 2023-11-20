918d4e63ccda74a85f216371221588f10d566cab
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-20 19:18:46 -0300

    Adicionando rota de usuário ao barrel

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,12 @@
 const mealsRouter = require('./meals.routes');
 const drinksRouter = require('./drinks.routes');
 const favoritesRouter = require('./favorites.routes');
+const usersRouter = require('./users.routes');
 
 
 module.exports = {
   mealsRouter,
   drinksRouter,
   favoritesRouter,
+  usersRouter,
 }
