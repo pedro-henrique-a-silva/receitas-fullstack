420fc11de7e6689b6796c2bdfabaadd7d0d6e75f
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-20 19:18:32 -0300

    adicionando model de usuário ao barrel

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,10 +1,12 @@
 const mealsModel = require('./meals.model');
 const drinksModel = require('./drinks.model');
 const favoritesModel = require('./favorites.model');
+const usersModel = require('./user.model');
 
 
 module.exports = {
   mealsModel,
   drinksModel,
   favoritesModel,
+  usersModel,
 }
