71b00bc864341b05a7849888d7d445e6b7301f45
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-20 18:51:31 -0300

    Criando camada model de favoritos

---
+++ backend/src/models/favorites.model.js
@@ -0,0 +1,22 @@
+const connection = require('./connection');
+const camelize = require('camelize');
+
+const getAll = async (userId) => {
+  const [favorites] = await connection.execute(`
+  SELECT * FROM favorites f
+  INNER JOIN users u ON u.id = f.user_id
+  LEFT JOIN (SELECT * FROM meals
+    INNER JOIN meals_categories mc 
+    ON meals.id_category = mc.id) m ON f.id_meal = m.id_meal
+  LEFT JOIN (SELECT * FROM drinks
+      INNER JOIN drinks_categories mc 
+      ON drinks.id_category = mc.id) d on f.id_drink = d.id_drink
+  WHERE user_id = ?;
+  `, [userId])
+
+  return camelize(favorites);
+}
+
+module.exports = {
+  getAll,
+}
