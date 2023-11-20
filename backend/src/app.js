2f29b83f3f4de9acf443df14ffc272b994849fa7
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-11-20 19:19:27 -0300

    adicionando rota de usuário ao app

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,9 +4,11 @@ const routes = require('./routes');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.use('/meals', routes.mealsRouter);
 app.use('/drinks', routes.drinksRouter);
 app.use('/favorites', routes.favoritesRouter);
+app.use('/users', routes.usersRouter);
 
 module.exports = app;
