d97b9a1ba45cb9e2530a70929b45ee9abf1821a0
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-05 19:49:22 -0300

    configurando sequelize

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,8 +1,8 @@
 const config = {
-  username: "root",
-  password: null,
-  database: "database_development",
-  host: "127.0.0.1",
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
   dialect: "mysql"
 }
   
