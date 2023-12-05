607a98fee9ea2f7f3e4128ec5d40f480505cf7a7
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-05 19:10:35 -0300

    Instalando e configurando o sequelize na api

---
+++ backend/src/config/config.js
@@ -0,0 +1,14 @@
+const config = {
+  username: "root",
+  password: null,
+  database: "database_development",
+  host: "127.0.0.1",
+  dialect: "mysql"
+}
+  
+
+module.exports = {
+  development: config,
+  tests: config,
+  production: config
+}
