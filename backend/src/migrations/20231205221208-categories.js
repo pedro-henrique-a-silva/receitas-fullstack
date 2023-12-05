ebec3320f1e1ee53ff3ac9af4655356119160881
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-05 20:49:41 -0300

    Corrigindo alguns tipos

--- backend/src/migrations/20231205221208-categories.js
+++ backend/src/migrations/20231205221208-categories.js
@@ -1,26 +1,24 @@
-'use strict';
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.createTable('categories', {
        id: {
         allowNull: false,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       category_name: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       category_type: {
         allowNull: false,
-        type: Sequelize.STRING
-      }
+        type: Sequelize.STRING,
+      },
       });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('categories');
-  }
+  },
 };
