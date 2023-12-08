3670b5115261cb0689927ba9487512bbec7f996d
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-08 15:06:47 -0300

    Criando service meal getaAllCategories

--- backend/src/services/meals.service.js
+++ backend/src/services/meals.service.js
@@ -1,10 +1,19 @@
-const { Recipe } = require('../models')
+const { Recipe, Category } = require('../models')
 
 const getAll = async () => {
   const allMeals = await Recipe.findAll({where : {recipeType: 'meal'}})
   return allMeals
 }
 
+const getAllCategories = async () => {
+  const allMealsCategories = await Category.findAll({
+    attributes: [ 'categoryName'],
+    where : {categoryType: 'meal'}
+  })
+  return allMealsCategories
+}
+
 module.exports = {
   getAll,
+  getAllCategories
 }
