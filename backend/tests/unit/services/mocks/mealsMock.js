d9cef42e8875c78007d21565c0d254d47f2ba554
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-08 17:26:16 -0300

    Mock busca por id

--- backend/tests/unit/services/mocks/mealsMock.js
+++ backend/tests/unit/services/mocks/mealsMock.js
@@ -1542,8 +1542,66 @@ const allMealsByCategoryFromDB = [
   }
 ]
 
+const mealByIdFromDB = {
+  "id": 53013,
+  "strName": "Big Mac",
+  "idCategory": 1,
+  "recipeType": "meal",
+  "strAlcoholic": null,
+  "strGlass": null,
+  "strArea": "American",
+  "strInstructions": "For the Big Mac sauce, combine all the ingredients in a bowl, season with salt and chill until ready to use.\r\n2. To make the patties, season the mince with salt and pepper and form into 4 balls using about 1/3 cup mince each. Place each onto a square of baking paper and flatten to form into four x 15cm circles. Heat oil in a large frypan over high heat. In 2 batches, cook beef patties for 1-2 minutes each side until lightly charred and cooked through. Remove from heat and keep warm. Repeat with remaining two patties.\r\n3. Carefully slice each burger bun into three acrossways, then lightly toast.\r\n4. To assemble the burgers, spread a little Big Mac sauce over the bottom base. Top with some chopped onion, shredded lettuce, slice of cheese, beef patty and some pickle slices. Top with the middle bun layer, and spread with more Big Mac sauce, onion, lettuce, pickles, beef patty and then finish with more sauce. Top with burger lid to serve.\r\n5. After waiting half an hour for your food to settle, go for a jog.",
+  "strThumb": "https://www.themealdb.com/images/media/meals/urzj1d1587670726.jpg",
+  "strTags": null,
+  "strYoutube": "https://www.youtube.com/watch?v=C5J39YnnPsg",
+  "strIngredient1": "Minced Beef",
+  "strIngredient2": "Olive Oil",
+  "strIngredient3": "Sesame Seed Burger Buns",
+  "strIngredient4": "Onion",
+  "strIngredient5": "Iceberg Lettuce",
+  "strIngredient6": "Cheese",
+  "strIngredient7": "Dill Pickles",
+  "strIngredient8": "Mayonnaise",
+  "strIngredient9": "White Wine Vinegar",
+  "strIngredient10": "Pepper",
+  "strIngredient11": "Mustard",
+  "strIngredient12": "Onion Salt",
+  "strIngredient13": "Garlic Powder",
+  "strIngredient14": "Paprika",
+  "strIngredient15": null,
+  "strIngredient16": null,
+  "strIngredient17": null,
+  "strIngredient18": null,
+  "strIngredient19": null,
+  "strIngredient20": null,
+  "strMeasure1": "400g",
+  "strMeasure2": "2 tbs",
+  "strMeasure3": "2",
+  "strMeasure4": "Chopped",
+  "strMeasure5": "1/4 ",
+  "strMeasure6": "2 sliced",
+  "strMeasure7": "2 large",
+  "strMeasure8": "1 cup ",
+  "strMeasure9": "2 tsp",
+  "strMeasure10": "Pinch",
+  "strMeasure11": "2 tsp",
+  "strMeasure12": "1 1/2 tsp ",
+  "strMeasure13": "1 1/2 tsp ",
+  "strMeasure14": "1/2 tsp",
+  "strMeasure15": null,
+  "strMeasure16": null,
+  "strMeasure17": null,
+  "strMeasure18": null,
+  "strMeasure19": null,
+  "strMeasure20": null,
+  "category": {
+    "categoryName": "Beef"
+  }
+}
+
 module.exports = {
   allMealsFromDB,
   allMealsCategoryFromDB,
-  allMealsByCategoryFromDB
+  allMealsByCategoryFromDB,
+  mealByIdFromDB
 }
