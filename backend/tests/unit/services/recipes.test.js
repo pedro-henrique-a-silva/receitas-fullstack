7afae5c168a42436e164e210c456ddb6c45bba6d
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-08 15:07:42 -0300

    Testes de categories comidas e bebidas

--- backend/tests/unit/services/recipes.test.js
+++ backend/tests/unit/services/recipes.test.js
@@ -7,12 +7,12 @@ const { expect } = chai
 const mealsMocks = require('./mocks/mealsMock')
 const drinksMocks = require('./mocks/drinksMock')
 
-const { Recipe } = require('../../../src/models')
+const { Recipe, Category } = require('../../../src/models')
 
 const { mealsService, drinksService } = require('../../../src/services');
-const { afterEach } = require('node:test');
+const { afterEach, describe } = require('node:test');
 
-describe("Tesando recipes - SERVICE", function () {
+describe("Tesando comidas - SERVICE", function () {
 
   it("Testando busca por todas as comidas", async function () {
     const recipeModel = sinon.stub(Recipe, 'findAll').resolves(mealsMocks.allMealsFromDB);
@@ -22,6 +22,20 @@ describe("Tesando recipes - SERVICE", function () {
     expect(recipeModel).to.have.been.calledWith({where : {recipeType: 'meal'}})
   })
 
+  it("Testando busca por todas as categórias de comidas", async function () {
+    const recipeModel = sinon.stub(Category, 'findAll').resolves(mealsMocks.allMealsCategoryFromDB);
+    const allMealsCategories = await mealsService.getAllCategories();
+
+    expect(allMealsCategories).to.be.deep.equal(mealsMocks.allMealsCategoryFromDB)
+    expect(recipeModel).to.have.been.calledWith({
+      attributes: [ 'categoryName'],
+      where : {categoryType: 'meal'}
+    })
+  })
+  beforeEach(sinon.restore)
+})
+
+describe('Testando bebidas - SERVICE', function () {
   it("Testando busca por todas as bebidas", async function () {
     const recipeModel = sinon.stub(Recipe, 'findAll').resolves(drinksMocks.allDrinksFromDB);
     const allDrink = await drinksService.getAll();
@@ -30,5 +44,15 @@ describe("Tesando recipes - SERVICE", function () {
     expect(recipeModel).to.have.been.calledWith({where : {recipeType: 'drink'}})
   })
 
+  it("Testando busca por todas as categórias de bebidas", async function () {
+    const recipeModel = sinon.stub(Category, 'findAll').resolves(drinksMocks.allDrinksCategoryFromDB);
+    const allDrinksCategories = await drinksService.getAllCategories();
+
+    expect(allDrinksCategories).to.be.deep.equal(drinksMocks.allDrinksCategoryFromDB)
+    expect(recipeModel).to.have.been.calledWith({
+      attributes: [ 'categoryName'],
+      where : {categoryType: 'drink'}
+    })
+  })
   beforeEach(sinon.restore)
 })
