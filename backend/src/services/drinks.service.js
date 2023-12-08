f1f73a7fb1d764a5592c6b90bca009260ab87868
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-08 15:06:27 -0300

    Criando service drink getAllCategories

--- backend/src/services/drinks.service.js
+++ backend/src/services/drinks.service.js
@@ -1,10 +1,19 @@
-const { Recipe } = require('../models')
+const { Recipe, Category } = require('../models')
 
 const getAll = async () => {
   const allDrinks = await Recipe.findAll({where: {recipeType: 'drink'}})
   return allDrinks
 }
 
+const getAllCategories = async () => {
+  const allDrinksCategories = await Category.findAll({
+    attributes: [ 'categoryName'],
+    where : {categoryType: 'drink'}
+  })
+  return allDrinksCategories
+}
+
 module.exports = {
   getAll,
+  getAllCategories
 }
