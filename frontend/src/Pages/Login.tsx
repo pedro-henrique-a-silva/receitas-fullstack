69d4521da0ad1ee23c4865dccf250a3f8ca1fbbf
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-09 18:55:38 -0300

    Criando formulario de registro de usuario

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -3,20 +3,36 @@ import { useNavigate } from 'react-router-dom';
 import { TextField, Button, Fade } from '@mui/material';
 import { CookingPot } from '@phosphor-icons/react';
 import { MainLogin, LoginWrapper } from './LoginStyle';
-import { LoginType } from '../types';
+import { LoginType, RegisterType } from '../types';
 
 const LOGIN_INITIAL_STATE = {
   email: '',
   password: '',
 };
 
+const REGISTER_INITIAL_STATE = {
+  name: '',
+  email: '',
+  password: '',
+};
+
 function Login() {
   const [loginUser, setLoginUser] = useState<LoginType>(LOGIN_INITIAL_STATE);
+  const [registerUser, setRegisterUser] = useState<RegisterType>(REGISTER_INITIAL_STATE);
+  const [isLogin, setIsLogin] = useState(true);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setLoginUser({
       ...loginUser,
+      [name]: value,
+    });
+  };
+
+  const handleChangeRegister = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setRegisterUser({
+      ...registerUser,
       [name]: value,
     });
   };
@@ -30,6 +46,22 @@ function Login() {
     }
     return regexEmail.test(loginUser.email);
   };
+
+  const validateFormRegister = (): boolean => {
+    const passwordLength = registerUser.password.length;
+    const nameLength = registerUser.name.length;
+    const regexEmail = /[^\s@]+@[^\s@]+\.[^\s@]+/gi;
+
+    if (passwordLength < 7 || nameLength < 5) {
+      return false;
+    }
+
+    return regexEmail.test(registerUser.email);
+  };
+
+  const changeSignUp = () => {
+    setIsLogin((prev) => !prev)
+  }
 
   const navigate = useNavigate();
   const toMealsAfterClick = () => navigate('/meals');
@@ -48,9 +80,78 @@ function Login() {
       <Fade in appear>
         <LoginWrapper elevation={ 5 }>
           <CookingPot size={ 80 } color="#2E2819" weight="fill" />
-          <form
+
+          {(isLogin) 
+          ?
+            (<form
+              onSubmit={ onSubmit }
+            >
+              <TextField
+                label="Email"
+                variant="outlined"
+                data-testid="email-input"
+                type="text"
+                id="email"
+                name="email"
+                value={ loginUser.email }
+                onChange={ handleChange }
+                size="small"
+                margin="dense"
+                sx={ { mb: 2 } }
+                autoComplete="off"
+              />
+  
+              <TextField
+                label="Senha"
+                variant="outlined"
+                data-testid="password-input"
+                type="password"
+                id="password"
+                name="password"
+                value={ loginUser.password }
+                onChange={ handleChange }
+                size="small"
+                margin="dense"
+                sx={ { mb: 2 } }
+              />
+  
+              <Button
+                variant="contained"
+                data-testid="login-submit-btn"
+                disabled={ !validateForm() }
+                type="submit"
+                color="secondary"
+              >
+                Entrar
+              </Button>
+              <Button
+                variant="text"
+                data-testid="register-change-btn"
+                type="button"
+                color="secondary"
+                onClick={changeSignUp}
+              >
+                inscrever-se
+              </Button>
+            </form>)
+          : (<form
             onSubmit={ onSubmit }
           >
+            <TextField
+              label="Nome"
+              variant="outlined"
+              data-testid="name-input"
+              type="text"
+              id="name"
+              name="name"
+              value={ registerUser.name }
+              onChange={ handleChangeRegister }
+              size="small"
+              margin="dense"
+              sx={ { mb: 2 } }
+              autoComplete="off"
+            />
+
             <TextField
               label="Email"
               variant="outlined"
@@ -58,8 +159,8 @@ function Login() {
               type="text"
               id="email"
               name="email"
-              value={ loginUser.email }
-              onChange={ handleChange }
+              value={ registerUser.email }
+              onChange={ handleChangeRegister }
               size="small"
               margin="dense"
               sx={ { mb: 2 } }
@@ -73,8 +174,8 @@ function Login() {
               type="password"
               id="password"
               name="password"
-              value={ loginUser.password }
-              onChange={ handleChange }
+              value={ registerUser.password }
+              onChange={ handleChangeRegister }
               size="small"
               margin="dense"
               sx={ { mb: 2 } }
@@ -83,13 +184,24 @@ function Login() {
             <Button
               variant="contained"
               data-testid="login-submit-btn"
-              disabled={ !validateForm() }
+              disabled={ !validateFormRegister() }
               type="submit"
               color="secondary"
             >
-              Enter
+              Inscrever-se
             </Button>
-          </form>
+            <Button
+                variant="text"
+                data-testid="login-change-btn"
+                type="button"
+                color="secondary"
+                onClick={changeSignUp}
+              >
+                Ja tenho uma conta.
+              </Button>
+          </form>)
+          }
+          
         </LoginWrapper>
       </Fade>
     </MainLogin>
