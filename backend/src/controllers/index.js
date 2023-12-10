9c2a1ca5d226736da7f49d76e5e57b97cb3607fd
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-10 16:46:24 -0300

    Add controller de user ao barrel

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,7 +1,9 @@
 const drinkController = require('./drinks.controller');
 const mealController = require('./meals.controller');
+const userController = require('./users.controller');
 
 module.exports = {
   drinkController,
-  mealController
+  mealController,
+  userController
 }
