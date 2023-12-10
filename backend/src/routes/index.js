2d630e33397133b40c813a5bf3f0a05a321baf75
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-09 21:07:20 -0300

    Add rota de user ao barrel

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,9 @@
 const mealsRouter = require('./meals.routes');
 const drinksRouter = require('./drinks.routes');
+const userRouter = require('./user.routes');
 
 module.exports = {
   mealsRouter,
   drinksRouter,
+  userRouter
 }
