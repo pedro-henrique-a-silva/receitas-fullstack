ab6174aa5637b417603ac0c67114aea75f693fbc
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-10 16:42:50 -0300

    Migrando rota para usar controller

--- backend/src/routes/meals.routes.js
+++ backend/src/routes/meals.routes.js
@@ -1,32 +1,14 @@
 const express = require('express');
-const { mealsService } = require('../services/index')
+const { mealController } = require('../controllers')
 const mealsRouter = express.Router();
-const httpMap = require('../utils/httpMap')
 
-mealsRouter.get('/all', async (req, res) => {
-  const allMeals = await mealsService.getAll();
+mealsRouter.get('/all', mealController.all)
 
-  return res.status(httpMap[allMeals.status]).json(allMeals.message);
-})
+mealsRouter.get('/all/categories', mealController.allCategories)
 
-mealsRouter.get('/all/categories', async (req, res) => {
-  const allCategories = await mealsService.getAllCategories();
+mealsRouter.get('/category/:categoryName', mealController.findByCategoryName)
 
-  return res.status(httpMap[allCategories.status]).json(allCategories.message);
-})
-
-mealsRouter.get('/category/:categoryName', async (req, res) => {
-  const { categoryName } = req.params;
-  const filteredMeals = await mealsService.findByCategory(categoryName);
-
-  return res.status(httpMap[filteredMeals.status]).json(filteredMeals.message);
-})
-
-mealsRouter.get('/:mealsId', async (req, res) => {
-  const { mealsId } = req.params;
-  const meal = await mealsService.findById(mealsId);
-  return res.status(httpMap[meal.status]).json(meal.message);
-})
+mealsRouter.get('/:mealsId', mealController.findById)
 
 
 
