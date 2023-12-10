f029c94fd42b47eaec1acefa315e8803995ddee9
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-10 19:30:42 -0300

    add rota favoritos ao barrel

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
 const mealsRouter = require('./meals.routes');
 const drinksRouter = require('./drinks.routes');
 const userRouter = require('./user.routes');
+const favoritesRouter = require('./favorites.routes')
 
 module.exports = {
   mealsRouter,
   drinksRouter,
-  userRouter
+  userRouter,
+  favoritesRouter
 }
