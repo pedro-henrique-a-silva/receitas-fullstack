5aa4a353014eb8361d1fa18c05d5e4e7bea2a024
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-10 19:31:28 -0300

    Add service favoritos ao barrel

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,9 +1,11 @@
 const mealsService = require('./meals.service');
 const drinksService = require('./drinks.service');
 const userService = require('./user.service');
+const favoritesService = require('./favorites.service')
 
 module.exports = {
   mealsService,
   drinksService,
-  userService
+  userService,
+  favoritesService,
 }
