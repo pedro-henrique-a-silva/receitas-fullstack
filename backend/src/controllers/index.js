8751ef4e57873501ee8ff6a6c7719facce502f92
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-10 19:29:13 -0300

    Add controllers de favoritos ao barrel

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,9 +1,11 @@
 const drinkController = require('./drinks.controller');
 const mealController = require('./meals.controller');
 const userController = require('./users.controller');
+const favoritesController = require('./favorites.controller')
 
 module.exports = {
   drinkController,
   mealController,
-  userController
+  userController,
+  favoritesController,
 }
