f4bf00608c732526e8b6cece3020291e4db4199f
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-10 16:43:05 -0300

    Add controller de meal ao barrel

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,5 +1,7 @@
 const drinkController = require('./drinks.controller');
+const mealController = require('./meals.controller');
 
 module.exports = {
-  drinkController
+  drinkController,
+  mealController
 }
