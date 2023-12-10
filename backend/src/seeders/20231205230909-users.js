c49cee8a05f229c3cf7ac12c6a31b2b479c5d832
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-09 21:07:42 -0300

    add nova coluna ao migration

--- backend/src/seeders/20231205230909-users.js
+++ backend/src/seeders/20231205230909-users.js
@@ -2,8 +2,8 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
    await queryInterface.bulkInsert('users', [
-    { id: 1, username: 'email@email.com', password: '1234567890' },
-    { id: 2, username: 'email2@email.com', password: '1234567890' },
+    { id: 1, name: "user01" , username: 'email@email.com', password: '1234567890' },
+    { id: 2, name: "user02", username: 'email2@email.com', password: '1234567890' },
    ]);
   },
 
