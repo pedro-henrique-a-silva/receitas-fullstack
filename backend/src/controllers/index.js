eecf6da58bd4edf4034bf7df53cfc28a9a31815c
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-11 15:57:26 -0300

    Add doneRecipes ao barrel de controllers

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -2,10 +2,12 @@ const drinkController = require('./drinks.controller');
 const mealController = require('./meals.controller');
 const userController = require('./users.controller');
 const favoritesController = require('./favorites.controller')
+const donesController = require('./done.controller')
 
 module.exports = {
   drinkController,
   mealController,
   userController,
   favoritesController,
+  donesController
 }
