261374480c28692f46823cb4ae9105f8855463c4
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-11 15:58:42 -0300

    Add done recipes ao barrel

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -2,10 +2,12 @@ const mealsService = require('./meals.service');
 const drinksService = require('./drinks.service');
 const userService = require('./user.service');
 const favoritesService = require('./favorites.service')
+const donesService = require('./dones.service')
 
 module.exports = {
   mealsService,
   drinksService,
   userService,
   favoritesService,
+  donesService
 }
