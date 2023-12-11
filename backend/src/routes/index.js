274e12e20fd44ee410339911de919e6580e04a7d
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-11 15:57:58 -0300

    Add doneRecipes router ao barrel

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,10 +2,12 @@ const mealsRouter = require('./meals.routes');
 const drinksRouter = require('./drinks.routes');
 const userRouter = require('./user.routes');
 const favoritesRouter = require('./favorites.routes')
+const doneRouter = require('./dones.routes')
 
 module.exports = {
   mealsRouter,
   drinksRouter,
   userRouter,
-  favoritesRouter
+  favoritesRouter,
+  doneRouter
 }
