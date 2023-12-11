f292dee7719b09e5fafa842e7c05f4ddc0028c93
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-12-10 21:23:02 -0300

    Controller updateFavorite

--- backend/src/controllers/favorites.controller.js
+++ backend/src/controllers/favorites.controller.js
@@ -2,12 +2,21 @@ const { favoritesService } = require('../services')
 const httpMap = require('../utils/httpMap')
 
 const getFavorites = async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.user;
   const favorites = await favoritesService.getFavorites(id);
+
+  return res.status(httpMap[favorites.status]).json(favorites.message)
+}
+
+const updateFavorites = async (req, res) => {
+  const { recipeId } = req.body;
+  const { id } = req.user;
+  const favorites = await favoritesService.updateFavorites(recipeId, id);
 
   return res.status(httpMap[favorites.status]).json(favorites.message)
 }
 
 module.exports = {
   getFavorites,
+  updateFavorites,
 }
