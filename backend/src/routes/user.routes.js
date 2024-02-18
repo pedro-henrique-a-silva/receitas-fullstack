3850ca77f3c37dd4383ae803aa42052581d6479b
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-18 11:11:47 -0300

    Iniciando backend com nest e prisma

--- backend/src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const {userController} = require('../controllers')
-const userRouter = express.Router();
-
-userRouter.post('/login', userController.login)
-
-userRouter.post('/signup', userController.signup)
-
-module.exports = userRouter;
--- backend/src/server.js
+++
@@ -1,5 +0,0 @@
-const app = require('./app');
-
-app.listen(3001, () => {
-  console.log('to vivo');
-})
