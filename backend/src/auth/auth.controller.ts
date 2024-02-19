fe6634bb7d890787bb35678a179cdcc3e3a7460f
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-19 17:24:49 -0300

    Create route signup

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthLoginDto } from './auth.dto';
+import { AuthLoginDto, SignUpDTO } from './auth.dto';
 
 @Controller('user')
 export class AuthController {
@@ -10,5 +10,11 @@ export class AuthController {
   async login(@Body() body: AuthLoginDto) {
     const user = await this.authService.auth(body);
     return user;
+  }
+
+  @Post('signup')
+  async signUp(@Body() body: SignUpDTO) {
+    const userCreated = await this.authService.signUp(body);
+    return userCreated;
   }
 }
