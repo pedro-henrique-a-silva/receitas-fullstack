419d4a582052a4be90928d39fb9d2c53453e9f11
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-19 17:27:07 -0300

    Add import of JwtModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import UsersModule from '../repositories/users/users.module';
+import { JwtModule } from '../commom/jwt/jwt.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, JwtModule],
   providers: [AuthService],
   controllers: [AuthController],
 })
