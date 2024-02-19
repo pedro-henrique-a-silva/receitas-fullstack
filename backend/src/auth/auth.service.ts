38d64adb7adc26a7627525952d39f888db8ea9fd
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-19 17:27:33 -0300

    Make validation & returning jwt

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,15 +1,39 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from '../repositories/users/users.service';
-import { IUserLoginDTO } from 'src/Interfaces/users/IUsers';
+import { IUserCreateDTO, IUserLoginDTO } from 'src/Interfaces/users/IUsers';
+import { JwtService } from '../commom/jwt/jwt.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async auth(loginData: IUserLoginDTO) {
     const user = await this.usersService.findOne(loginData);
     if (!user) throw new NotFoundException();
 
-    return user;
+    const token = this.jwtService.sign({
+      name: user.name,
+      username: user.username,
+    });
+    return { token };
+  }
+
+  async signUp(signUpData: IUserCreateDTO) {
+    const user = await this.usersService.findOne({
+      username: signUpData.username,
+    });
+
+    if (user) throw new ConflictException();
+
+    const userCreated = await this.usersService.create(signUpData);
+
+    return userCreated;
   }
 }
