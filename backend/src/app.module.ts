a0aa6e743e81301830b2e6ee902ee2425b63ff9c
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-19 17:24:33 -0300

    Add JWT import

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import UsersModule from './users/users.module';
+import UsersModule from './repositories/users/users.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from './commom/jwt/jwt.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, JwtModule],
   controllers: [AppController],
   providers: [AppService],
 })
