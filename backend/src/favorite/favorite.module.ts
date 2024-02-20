07fd62faaeaa3b9fbe1d9289cd974d0d6134608f
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 17:39:39 -0300

    Implementing favorite.module

--- backend/src/favorite/favorite.module.ts
+++ backend/src/favorite/favorite.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import FavoriteRepository from './favorite.repository';
 import { FavoriteService } from './favorite.service';
 import { FavoriteController } from './favorite.controller';
-import PrismaModule from 'src/prisma/prisma.module';
+import PrismaModule from '../prisma/prisma.module';
+import CommonModule from '../common/auth.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CommonModule, AuthModule],
   providers: [FavoriteRepository, FavoriteService],
   controllers: [FavoriteController],
 })
