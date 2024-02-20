1f7feff0427c23cb9601349d8a6b2dfff4a8a8a4
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 17:40:03 -0300

    implementing updateFavorite from repository

--- backend/src/favorite/favorite.repository.ts
+++ backend/src/favorite/favorite.repository.ts
@@ -21,4 +21,31 @@ export default class FavoriteRepository implements IFavoriteRepository {
 
     return favorites;
   }
+
+  async updateFavorites(recipeId: number, userId: number): Promise<void> {
+    const favorite = await this.prisma.favoriteRecipes.findFirst({
+      where: { recipeId, userId },
+    });
+
+    if (favorite) {
+      await this.prisma.favoriteRecipes.delete({
+        where: {
+          userId_recipeId: {
+            userId,
+            recipeId,
+          },
+        },
+      });
+      return;
+    }
+
+    await this.prisma.favoriteRecipes.create({
+      data: {
+        userId,
+        recipeId,
+      },
+    });
+
+    return;
+  }
 }
