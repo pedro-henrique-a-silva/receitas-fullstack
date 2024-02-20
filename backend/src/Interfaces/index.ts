0a7f7d44a60ef68fca1b55a445c6a503699269cc
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 19:06:37 -0300

    Refactoring global interfaces

--- backend/src/Interfaces/index.ts
+++ backend/src/Interfaces/index.ts
@@ -1,5 +1,16 @@
+import { Request } from 'express';
+
 export type NewEntity<T> = Omit<T, 'id'>;
 
 export type ID = number;
 
 export type Identifiable = { id: ID };
+
+export interface RequestWithUserDataJWT extends Request {
+  user: {
+    name: string;
+    username: string;
+    iat: number;
+    exp: number;
+  };
+}
