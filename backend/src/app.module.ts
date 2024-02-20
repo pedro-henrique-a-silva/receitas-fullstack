9cddf8f90172bc9bf1f9bccc30407cf506223857
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 17:37:20 -0300

    Configure jwtModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,19 @@ import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { FavoriteModule } from './favorite/favorite.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [FavoriteModule, AuthModule, RecipesModule],
+  imports: [
+    FavoriteModule,
+    AuthModule,
+    RecipesModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET || 'secret',
+      signOptions: { expiresIn: '1d', algorithm: 'HS256' },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
