42e74d9c8acccc8d023ee9477279d517413b5d88
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 17:41:12 -0300

    Add updateFavorite in IFavoriteRepository interface

--- backend/src/favorite/interface/IFavoriteRepository.ts
+++ backend/src/favorite/interface/IFavoriteRepository.ts
@@ -2,4 +2,5 @@ import { AllUserFavorites } from './favorite.interface';
 
 export interface IFavoriteRepository {
   getFavorites(id: number): Promise<AllUserFavorites | null>;
+  updateFavorites(recipeId: number, userId: number): Promise<void>;
 }
