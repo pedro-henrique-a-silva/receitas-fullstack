f4b9471713dd66aee9a3dc86288c422dc465fbfc
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 11:41:08 -0300

    Creating getAll recipes & getById route

--- backend/src/recipes/recipes.controller.ts
+++ backend/src/recipes/recipes.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { RecipesService } from './recipes.service';
 
-@Controller('recipes')
-export class RecipesController {}
+@Controller('')
+export class RecipesController {
+  constructor(private recipesService: RecipesService) {}
+
+  @Get(':recipeType')
+  async getAll(@Param('recipeType') recipeType: string) {
+    const recipes = await this.recipesService.findAll(recipeType);
+
+    return recipes;
+  }
+
+  @Get(':recipeType/:id')
+  async getById(
+    @Param('recipeType') recipeType: string,
+    @Param('id') id: string,
+  ) {
+    const recipe = await this.recipesService.findById(Number(id), recipeType);
+    return recipe;
+  }
+}
