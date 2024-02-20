4b69557d7ff47b17ee6e6e96b4da3a1f385dd4ca
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 10:32:12 -0300

    Creating IUserRepositore

---
+++ backend/src/auth/interfaces/IUserRepositore.ts
@@ -0,0 +1,8 @@
+import { ICRUDModelCreator } from '../../Interfaces/ICRUDModel';
+import { Prisma, User } from '@prisma/client';
+
+export interface IUserRepositore
+  extends ICRUDModelCreator<Prisma.UserCreateInput> {
+  findOne(searchData: Prisma.UserWhereInput): Promise<User | null>;
+  // create(signUpData: Prisma.UserCreateInput): Promise<User | null>;
+}
