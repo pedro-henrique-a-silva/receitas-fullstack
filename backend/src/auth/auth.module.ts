661e1a714714431ef391312a8d72061ea60e8091
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 11:39:32 -0300

    Fixing import of Prisma module and UserRepository

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '../commom/jwt/jwt.module';
+import { UsersRepository } from './users.repository';
+import PrismaModule from '../prisma/prisma.module';
 
 @Module({
-  imports: [JwtModule],
-  providers: [AuthService],
+  imports: [JwtModule, PrismaModule],
+  providers: [AuthService, UsersRepository],
   controllers: [AuthController],
 })
 export class AuthModule {}
