962acf6bed99fecbd67e03961e2df79988049a13
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 19:03:13 -0300

    Implementing dones module

--- backend/src/dones/dones.module.ts
+++ backend/src/dones/dones.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { DonesService } from './dones.service';
+import DonesRepository from './dones.repository';
+import { DonesController } from './dones.controller';
+import { AuthModule } from '../auth/auth.module';
+import PrismaModule from '../prisma/prisma.module';
+import CommonModule from '../common/auth.module';
 
-@Module({})
+@Module({
+  imports: [PrismaModule, CommonModule, AuthModule],
+  providers: [DonesService, DonesRepository],
+  controllers: [DonesController],
+})
 export class DonesModule {}
