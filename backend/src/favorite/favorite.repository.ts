529d8ea2f95b64935f305418ad5755849a6cceb1
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 14:26:15 -0300

    Implementing IFavoriteRepository interface

--- backend/src/favorite/favorite.repository.ts
+++ backend/src/favorite/favorite.repository.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AllUserFavorites } from './interface/favorite.interface';
+import { IFavoriteRepository } from './interface/IFavoriteRepository';
 
 @Injectable()
-export default class FavoriteRepository {
+export default class FavoriteRepository implements IFavoriteRepository {
   constructor(private prisma: PrismaService) {}
 
   async getFavorites(id: number): Promise<AllUserFavorites | null> {
