a5ecf931415b3ee20f05783e7a196694786dddab
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 19:03:41 -0300

    implementing dones.service

--- backend/src/dones/dones.service.ts
+++ backend/src/dones/dones.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import DonesRepository from './dones.repository';
+import { UsersRepository } from '../auth/users.repository';
+import { AllUserDones } from './interfaces/dones.interface';
 
 @Injectable()
-export class DonesService {}
+export class DonesService {
+  constructor(
+    private doneRepository: DonesRepository,
+    private userRepository: UsersRepository,
+  ) {}
+
+  async getDones(id: number): Promise<AllUserDones | null> {
+    const favorites = this.doneRepository.getDones(id);
+    return favorites;
+  }
+
+  async updateDones(
+    recipeId: number,
+    userId: number,
+    username: string,
+  ): Promise<void> {
+    const user = await this.userRepository.findOne({ username });
+
+    if (!user || user.id !== userId) throw new UnauthorizedException();
+
+    await this.doneRepository.updateDones(recipeId, userId);
+  }
+}
