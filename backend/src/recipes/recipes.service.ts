e40ea8954ff18f7caf5281aa8909f765b2571c4a
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 11:42:22 -0300

    Implementing findAll & findbyid

--- backend/src/recipes/recipes.service.ts
+++ backend/src/recipes/recipes.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Recipe } from '@prisma/client';
+import { RecipesRepository } from './recipes.repository';
 
 @Injectable()
-export class RecipesService {}
+export class RecipesService {
+  constructor(private recipesRepository: RecipesRepository) {}
+
+  async findAll(recipeType: string): Promise<Recipe[]> {
+    const recipes = await this.recipesRepository.findAll(recipeType);
+
+    return recipes;
+  }
+
+  async findById(id: number, recipeType: string): Promise<Recipe> {
+    const recipe = await this.recipesRepository.findById(id, recipeType);
+
+    if (!recipe) throw new NotFoundException();
+
+    return recipe;
+  }
+}
