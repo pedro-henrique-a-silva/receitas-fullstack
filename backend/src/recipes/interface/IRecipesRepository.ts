1c8c7a5c9441448e67520e773f7e407e5c356bf3
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 13:10:41 -0300

    Updating IRecipeRepository interface

--- backend/src/recipes/interface/IRecipesRepository.ts
+++ backend/src/recipes/interface/IRecipesRepository.ts
@@ -1,7 +1,11 @@
-import { Recipe } from '@prisma/client';
+import { Categorie, Recipe } from '@prisma/client';
 import { ID } from 'src/Interfaces';
 
 export interface IRecipeRepository {
-  findAll(type: string): Promise<Recipe[]>;
-  findById(id: ID, type: string): Promise<Recipe | null>;
+  findAll(recipeTypey: string): Promise<Recipe[]>;
+  findById(id: ID, recipeType: string): Promise<Recipe | null>;
+  findAllCategories(
+    recipeType: string,
+  ): Promise<Pick<Categorie, 'categoryName'>[]>;
+  findByCategory(recipeType: string, categoryName: string): Promise<Recipe[]>;
 }
