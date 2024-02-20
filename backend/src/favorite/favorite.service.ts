ad7ec6de2b482d9f035313d59b51a86e6e338fde
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 17:40:22 -0300

    implementing updateFavorite from service

--- backend/src/favorite/favorite.service.ts
+++ backend/src/favorite/favorite.service.ts
@@ -1,13 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import FavoriteRepository from './favorite.repository';
 import { AllUserFavorites } from './interface/favorite.interface';
+import { UsersRepository } from '../auth/users.repository';
 
 @Injectable()
 export class FavoriteService {
-  constructor(private favoriteRepository: FavoriteRepository) {}
+  constructor(
+    private favoriteRepository: FavoriteRepository,
+    private userRepository: UsersRepository,
+  ) {}
 
   async getFavorites(id: number): Promise<AllUserFavorites | null> {
     const favorites = this.favoriteRepository.getFavorites(id);
     return favorites;
+  }
+
+  async updateFavorites(
+    recipeId: number,
+    userId: number,
+    username: string,
+  ): Promise<void> {
+    const user = await this.userRepository.findOne({ username });
+
+    if (!user || user.id !== userId) throw new UnauthorizedException();
+
+    await this.favoriteRepository.updateFavorites(recipeId, userId);
   }
 }
