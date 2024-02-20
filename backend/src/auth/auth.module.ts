d7bda19ee2a1f66d6747f5bbd2e40eedb290486c
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 17:37:38 -0300

    Export UsersRepository

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtModule } from '../commom/jwt/jwt.module';
 import { UsersRepository } from './users.repository';
 import PrismaModule from '../prisma/prisma.module';
 
 @Module({
-  imports: [JwtModule, PrismaModule],
+  imports: [PrismaModule],
   providers: [AuthService, UsersRepository],
   controllers: [AuthController],
+  exports: [UsersRepository],
 })
 export class AuthModule {}
