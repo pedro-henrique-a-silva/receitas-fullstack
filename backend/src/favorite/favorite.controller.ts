415d2fc8e67b5bfce509f8e27ba99637dceecac5
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 17:39:22 -0300

    Implementing update favorite route

--- backend/src/favorite/favorite.controller.ts
+++ backend/src/favorite/favorite.controller.ts
@@ -1,13 +1,41 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { FavoriteService } from './favorite.service';
+import { AuthGuard } from 'src/common/auth.guard';
+import { RequestWithUserDataJWT } from './interface/favorite.interface';
 
 @Controller('favorites')
 export class FavoriteController {
   constructor(private favoriteService: FavoriteService) {}
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   async getFavorites(@Param('id') id: string) {
     const favorites = await this.favoriteService.getFavorites(Number(id));
     return favorites;
+  }
+
+  @UseGuards(AuthGuard)
+  @HttpCode(200)
+  @Post(':id')
+  async updateFavorite(
+    @Body('recipeId') recipeId: string,
+    @Param('id') userId: string,
+    @Req()
+    req: RequestWithUserDataJWT,
+  ) {
+    await this.favoriteService.updateFavorites(
+      Number(recipeId),
+      Number(userId),
+      req.user.username,
+    );
   }
 }
