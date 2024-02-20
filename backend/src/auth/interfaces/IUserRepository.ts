47343ff1f517a7e733f4b5ac1d482c85422a2641
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-20 11:40:30 -0300

    Formating IUserRepository

--- backend/src/auth/interfaces/IUserRepository.ts
+++ backend/src/auth/interfaces/IUserRepository.ts
@@ -4,5 +4,4 @@ import { Prisma, User } from '@prisma/client';
 export interface IUserRepositore
   extends ICRUDModelCreator<Prisma.UserCreateInput> {
   findOne(searchData: Prisma.UserWhereInput): Promise<User | null>;
-  // create(signUpData: Prisma.UserCreateInput): Promise<User | null>;
 }
