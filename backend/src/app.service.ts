5e9341f747cbfb93cc90526b1115be1ead3b29eb
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-25 11:27:58 -0300

    change message getHello

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  constructor() {}
   getHello(): string {
-    return `I'm alive!`;
+    return `I'm alive! Working on a nest server`;
   }
 }
