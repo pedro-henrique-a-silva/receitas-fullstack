f4b75e6cf136cf52134507d508afc819c93a03b7
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-25 11:29:14 -0300

    Using DI to use bcryptservice

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,22 +1,31 @@
 import {
+  BadRequestException,
   ConflictException,
   Injectable,
-  NotFoundException,
 } from '@nestjs/common';
-import { UsersRepository } from './users.repository';
 import { IUserCreateDTO, IUserLoginDTO } from './interfaces/auth.interface';
 import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import HashService from '../common/hash/hash.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private usersRepository: UsersRepository,
+    private userService: UsersService,
     private jwtService: JwtService,
+    private bCryptService: HashService,
   ) {}
 
   async auth(loginData: IUserLoginDTO) {
-    const user = await this.usersRepository.findOne(loginData);
-    if (!user) throw new NotFoundException();
+    const user = await this.userService.findByUsername(loginData.username);
+
+    if (!user) throw new BadRequestException();
+
+    const isPasswordMatch = await this.bCryptService.compare(
+      loginData.password,
+      user.password,
+    );
+    if (!isPasswordMatch) throw new BadRequestException();
 
     const token = await this.jwtService.signAsync({
       name: user.name,
@@ -27,13 +36,14 @@ export class AuthService {
   }
 
   async signUp(signUpData: IUserCreateDTO) {
-    const user = await this.usersRepository.findOne({
-      username: signUpData.username,
-    });
-
+    const user = await this.userService.findByUsername(signUpData.username);
     if (user) throw new ConflictException();
 
-    const userCreated = await this.usersRepository.create(signUpData);
+    const userCreated = await this.userService.create({
+      name: signUpData.name,
+      username: signUpData.username,
+      password: await this.bCryptService.hash(signUpData.password),
+    });
 
     return userCreated;
   }
