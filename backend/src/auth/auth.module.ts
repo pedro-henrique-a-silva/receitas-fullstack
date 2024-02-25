6c3d33d0523d6207dbc157d8bfbaa924f1dc0b28
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-02-25 11:29:35 -0300

    Importing commom module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersRepository } from './users.repository';
 import PrismaModule from '../prisma/prisma.module';
+import { UsersModule } from '../users/users.module';
+import CommonModule from '../common/common.module';
 
 @Module({
-  imports: [PrismaModule],
-  providers: [AuthService, UsersRepository],
+  imports: [PrismaModule, UsersModule, CommonModule],
+  providers: [AuthService],
   controllers: [AuthController],
-  exports: [UsersRepository],
 })
 export class AuthModule {}
