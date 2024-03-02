4e76f21da180cc4c5cb41c7f8af515177f1364fc
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-03-01 21:33:38 -0300

    implementing Recipe interfaces

--- backend-sequelize-ts/src/interfaces/recipe/IRecipe.ts
+++ backend-sequelize-ts/src/interfaces/recipe/IRecipe.ts
@@ -1,14 +1,15 @@
-import { Identifiable } from "..";
+import { Identifiable } from '..';
 
 export interface IRecipe extends Identifiable {
-    strName: string;
-    categoryId: number;
-    recipeType: string;
-    strAlcoholic: string;
-    strGlass: string;
-    strArea: string;
-    strInstructions: string;
-    strThumb: string;
-    strTags: string;
-    strYoutube: string;
+  strName: string;
+  categoryId: number;
+  recipeType: string;
+  strAlcoholic: string;
+  strGlass: string;
+  strArea: string;
+  strInstructions: string;
+  strThumb: string;
+  strTags: string;
+  strYoutube: string;
+  favorite?: boolean;
 }
