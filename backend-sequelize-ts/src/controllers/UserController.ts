d03cebd555841f775c037172ee04e3cb7e465d27
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-03-01 21:30:16 -0300

    Implementing UserController

---
+++ backend-sequelize-ts/src/controllers/UserController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import UserService from '../services/UserService';
+import mapStatusHTTP from '../utils/httpMap';
+
+export default class UserController {
+  constructor(
+    private userService: UserService = new UserService(),
+  ) {}
+
+  async login(req: Request, res: Response) {
+    const { username, password } = req.body;
+
+    const { status, data } = await this.userService.findByUsername(username);
+
+    const statusCode = mapStatusHTTP(status);
+
+    return res.status(statusCode).json(data);
+  }
+
+  async signup(req: Request, res: Response) {
+    const { name, username, password } = req.body;
+
+    const { status, data } = await this.userService.create({ name, username, password });
+
+    const statusCode = mapStatusHTTP(status);
+
+    return res.status(statusCode).json(data);
+  }
+}
