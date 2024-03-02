8ba8ce71923c19c655afeaadfbe4835b7b24b87e
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-03-01 21:35:07 -0300

    Implementing Done Service

---
+++ backend-sequelize-ts/src/services/DoneService.ts
@@ -0,0 +1,32 @@
+import IUserModel from '../interfaces/user/IUserModel';
+import IDonesModel from '../interfaces/dones/IDonesModel';
+import DoneModel from '../model/DoneModel';
+import UserModel from '../model/UserModel';
+
+export default class DoneService {
+  constructor(
+    private doneModel: IDonesModel = new DoneModel(),
+    private userModel: IUserModel = new UserModel(),
+  ) {}
+
+  async getDones(id: number) {
+    const dones = await this.doneModel.getDones(id);
+    return { status: 'SUCCESSFUL', data: dones };
+  }
+
+  async updateDones(
+    recipeId: number,
+    userId: number,
+    username: string,
+  ) {
+    const user = await this.userModel.findByUsername(username);
+
+    if (!user || user.id !== userId) {
+      return { status: 'UNAUTHORIZED', data: { message: 'unauthorized' } };
+    }
+
+    await this.doneModel.updateDones(recipeId, userId);
+
+    return { status: 'SUCCESSFUL', data: { message: 'favorited' } };
+  }
+}
