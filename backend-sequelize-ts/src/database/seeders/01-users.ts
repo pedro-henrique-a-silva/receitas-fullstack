061ae5921f6689550d26bf866c001845dfef1579
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-03-02 21:18:10 -0300

    hashing password

--- backend-sequelize-ts/src/database/seeders/01-users.ts
+++ backend-sequelize-ts/src/database/seeders/01-users.ts
@@ -1,10 +1,25 @@
 import { QueryInterface } from 'sequelize';
+import BCryptHashService from '../../utils/Hash';
+
+const hashService = new BCryptHashService()
 
 export default {
   up: async (queryInterface: QueryInterface) => {
     await queryInterface.bulkInsert('users', [
-      { id: 1, name: "user01" , username: 'email@email.com', password: '1234567890' },
-      { id: 2, name: "user02", username: 'email2@email.com', password: '1234567890' },
+      { 
+        id: 1, 
+        name: "user01" , 
+        username: 'email@email.com', 
+        password: hashService.hash('123456')
+        // password: '123456'
+      },
+      { 
+        id: 2, 
+        name: "user02", 
+        username: 'email2@email.com', 
+        password: hashService.hash('123456')
+        // password: '123456'
+      },
      ]);
   },
 
