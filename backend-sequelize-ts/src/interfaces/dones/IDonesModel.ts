adacdc27b24f37a4e677f6be08a572b73e15c431
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-03-03 18:06:22 -0300

    Refactoring interfaces

--- backend-sequelize-ts/src/interfaces/dones/IDonesModel.ts
+++ backend-sequelize-ts/src/interfaces/dones/IDonesModel.ts
@@ -2,5 +2,5 @@ import { AllUserDones, IDones } from './IDones';
 
 export default interface IDonesModel {
   getDones(id: number): Promise<AllUserDones | null>;
-  updateDones(recipeId: number, userId: number): Promise<IDones | null>;
+  updateDones(recipeId: number, userId: number): Promise<void>;
 }
--- backend-sequelize-ts/src/interfaces/favorite/IFavoriteModel.ts
+++ backend-sequelize-ts/src/interfaces/favorite/IFavoriteModel.ts
@@ -2,6 +2,6 @@ import { AllUserFavorites, IFavorite } from './IFavorite';
 
 export default interface IFavoriteModel {
   getFavorites(id: number): Promise<AllUserFavorites | null>;
-  updateFavorites(recipeId: number, userId: number): Promise<IFavorite | null>;
+  updateFavorites(recipeId: number, userId: number): Promise<void>;
   getOneFavorite(recipeId: number, userId: number): Promise<IFavorite | null>;
 }
