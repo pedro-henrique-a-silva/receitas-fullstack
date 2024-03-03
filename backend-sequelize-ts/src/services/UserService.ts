9057233c4c838f7bd872612e65c8428e1f6aaf2e
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-03-02 21:20:02 -0300

    Add HashService

--- backend-sequelize-ts/src/services/UserService.ts
+++ backend-sequelize-ts/src/services/UserService.ts
@@ -2,19 +2,29 @@ import { CreateUser, IUser } from '../interfaces/user/IUser';
 import IUserModel from '../interfaces/user/IUserModel';
 import UserModel from '../model/UserModel';
 import { ServiceResponse } from '../interfaces/ServiceResponse';
-
 import JWT from '../utils/JWT';
+import HashService from '../interfaces/IHash';
+import BCryptHashService from '../utils/Hash';
 
 export default class UserService {
+  private msgInvalidData = 'Dados incorretos';
   constructor(
     private userModel: IUserModel = new UserModel(),
     private jwtService = JWT,
+    private hashService: HashService = new BCryptHashService(),
   ) { }
 
-  async findByUsername(username: string): Promise<ServiceResponse<{ token: string }>> {
+  async findByUsername(
+    username: string,
+    password: string,
+  ): Promise<ServiceResponse<{ token: string }>> {
     const user = await this.userModel.findByUsername(username);
 
-    if (!user) return { status: 'INVALID_DATA', data: { message: 'Dados incorretos' } };
+    if (!user) return { status: 'INVALID_DATA', data: { message: this.msgInvalidData } };
+
+    const isPasswordMatch = await this.hashService.compare(password, user.password);
+
+    if (!isPasswordMatch) return { status: 'INVALID_DATA', data: { message: this.msgInvalidData } };
 
     const payload = {
       id: user.id,
@@ -32,7 +42,11 @@ export default class UserService {
 
     if (user) return { status: 'CONFLICT', data: { message: 'Dados incorretos' } };
 
-    const userCreated = await this.userModel.create(data);
+    const userCreated = await this.userModel.create({
+      name: data.name,
+      username: data.username,
+      password: await this.hashService.hash(data.password),
+    });
 
     return { status: 'CREATED', data: userCreated };
   }
