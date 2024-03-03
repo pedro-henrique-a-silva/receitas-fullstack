1d00bd289bfaa2847fd322d907068cccdf3bec54
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-03-02 21:19:30 -0300

    Add AuthMiddleware to routes

--- backend-sequelize-ts/src/routes/index.ts
+++ backend-sequelize-ts/src/routes/index.ts
@@ -3,10 +3,14 @@ import userRouter from './user.routes';
 import favoritesRouter from './favorites.routes';
 import doneRouter from './dones.routes';
 import recipesRouter from './recipes.routes';
+import Auth from '../middleware/Auth';
 
 const router = Router();
 
 router.use(userRouter);
+
+router.use(Auth.auth);
+
 router.use('/favorites', favoritesRouter);
 router.use('/dones', doneRouter);
 router.use(recipesRouter);
