5a84800ed39ce3463913dc3fe1725ff9ff9c8838
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-03-04 12:29:51 -0300

    Implementing DoneModel sequelize respecting contract

--- backend-sequelize-ts/src/model/prisma/DoneModel.ts
+++ backend-sequelize-ts/src/model/prisma/DoneModel.ts
@@ -1,25 +1,26 @@
 import { PrismaClient } from '@prisma/client';
 import IDonesModel from '../../interfaces/dones/IDonesModel';
-import { AllUserDones } from '../../interfaces/dones/IDones';
+import { AllUserDones, DonesFromDB } from '../../interfaces/dones/IDones';
+import getDonesQuerie from './queries/getDonesQuerie';
+import { RecipeList } from '../../interfaces/favorite/IFavorite';
 
 export default class DoneModel implements IDonesModel {
   constructor(
     private prisma = new PrismaClient(),
   ) {}
 
-  async getDones(id: number): Promise<any> {
-    const dones = await this.prisma.user.findUnique({
-      select: {
-        name: true,
-        username: true,
-        doneRecipes: {
-          select: { recipe: true },
-        },
-      },
-      where: { id },
-    });
+  async getDones(id: number): Promise<AllUserDones> {
+    const dones = await this.prisma.user.findUnique(getDonesQuerie(id));
 
-    return dones;
+    const { doneRecipes, ...restdones } = dones as never as DonesFromDB;
+
+    const donesRecipesList = doneRecipes
+      .map((recipe: RecipeList) => {
+        const { categoryId, category, ...restRecipe } = recipe.recipe;
+        return { ...restRecipe, categoryName: category.categoryName };
+      });
+
+    return { ...restdones, doneRecipes: donesRecipesList };
   }
 
   async updateDones(recipeId: number, userId: number): Promise<void> {
