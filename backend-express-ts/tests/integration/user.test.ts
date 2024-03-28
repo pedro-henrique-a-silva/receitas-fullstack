31186abe8244d46a3c8425646bf482520ea325fa
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-03-28 17:15:49 -0300

    Creating user Integration test file

---
+++ backend-express-ts/tests/integration/user.test.ts
@@ -0,0 +1,10 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+
+describe('LoginService', function () {
+  beforeEach(function () { sinon.restore(); });
+  it('Should return status SUCCESS and token', async  function () {
+  
+  });
+
+});
